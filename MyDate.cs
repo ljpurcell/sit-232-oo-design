8f5db07456e755b63d6d2e4b9ffa1ea90e382fc9
Lyndon Purcell <65317064+ljpurcell@users.noreply.github.com>
2022-07-23 16:00:16 +1000

    Upload assignment: MyDateTime

---
+++ MyDate.cs
@@ -0,0 +1,291 @@
+﻿using System;
+namespace MyDateTime
+{
+    public class MyDate
+    {
+        // Instance variables
+        private int _year = 1;
+        private int _month = 1;
+        private int _day = 1;
+
+        private enum MONTHS
+        { Jan = 1, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec };
+
+        private enum DAYS
+        { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday };
+
+        private static readonly int[] DAYS_IN_MONTHS
+            = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+        // Constructor
+        public MyDate(int day, int month, int year)
+        {
+            _day = day;
+            _month = month;
+            _year = year;
+        }
+
+        private int DayMax() // For current year and month
+        {
+            int currentMonth = GetMonth();
+            int currentYear = GetYear();
+            if (IsLeapYear(currentYear) && currentMonth == 2)
+                return 29;
+            else
+                return DAYS_IN_MONTHS[currentMonth - 1];
+                // MONTHS enum indexed to 1, but DAYS array to 0 
+        }
+
+        private int DayMax(int month, int year) // For different month and year
+        {
+            if (IsLeapYear(year) && month == 2)
+                return 29;
+            else
+            {
+                if (month >= 1 && month <= 12)
+                {
+                    return DAYS_IN_MONTHS[month - 1];
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Invalid month!");
+                }
+            }
+
+        }
+
+        public void SetYear(int year)
+        {
+            try
+            {
+                if (1 <= year && year <= 9999)
+                    _year = year;
+                else
+                    throw new ArgumentOutOfRangeException("Invalid year!");
+            }
+            catch(ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+        }
+
+        public void SetMonth(int month)
+        {
+            try
+            {
+                if (1 <= month && month <= 12)
+                    _month = month;
+                else
+                    throw new ArgumentOutOfRangeException("Invalid month!");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+        }
+
+        public void SetDay(int day)
+        {
+            try
+            {
+                if (1 <= day && day <= DayMax())
+                    _day = day;
+                else
+                    throw new ArgumentOutOfRangeException("Invalid day!");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+        }
+
+        public void SetDate(int day, int month, int year)
+        {
+            try
+            {
+                if (IsValidDate(day, month, year))
+                {
+                    SetYear(year);
+                    SetMonth(month);
+                    SetDay(day);
+                }
+                else
+                    throw new ArgumentOutOfRangeException("Invalid day, month, or year!");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+        }
+
+        public int GetYear()
+        {
+            return _year;
+        }
+
+        public int GetMonth()
+        {
+            return _month;
+        }
+
+        public int GetDay()
+        {
+            return _day;
+        }
+
+        public MyDate NextDay()
+        {
+            int currentDay = GetDay();
+            if (currentDay < DayMax())
+            {
+                SetDay(currentDay + 1);
+            }
+            else
+            {
+                SetDay(1);
+                NextMonth();
+            }
+            return this;
+        }
+
+        public MyDate NextMonth() 
+        {
+            int currentMonth = GetMonth();
+            if (currentMonth < 12)
+            {
+                int nextMonth = currentMonth + 1;
+                if (GetDay() > DayMax(nextMonth, GetYear()))
+                {
+                    SetDay(DayMax(nextMonth, GetYear()));
+                }
+                SetMonth(nextMonth);
+            }
+            else
+            {
+                SetMonth(1);
+                NextYear();
+            }
+            return this;
+        }
+
+        public MyDate NextYear() 
+        {
+            int currentYear = GetYear();
+            if (currentYear < 9999)
+            {
+                
+                if (GetDay() > DayMax())
+                { SetDay(DayMax()); }
+
+                int nextYear = currentYear + 1;
+                if (GetDay() > DayMax(GetMonth(), nextYear))
+                {
+                    SetDay(DayMax(GetMonth(), nextYear));
+                }
+
+                SetYear(currentYear + 1);
+
+                return this;
+            }
+            else
+            {
+                throw new InvalidOperationException("Year out of range!");
+            }
+        }
+    
+
+        public MyDate PreviousDay()
+        {
+            int currentDay = GetDay();
+            if (currentDay > 1)
+            {
+                SetDay(currentDay - 1);
+            }
+            else
+            {
+                PreviousMonth();
+                SetDay(DayMax());
+            }
+            return this;
+        }
+
+        public MyDate PreviousMonth()
+        {
+            int currentMonth = GetMonth();
+            if (currentMonth > 1)
+            {
+                int prevMonth = currentMonth - 1;
+                if (GetDay() > DayMax(prevMonth, GetYear()))
+                {
+                    SetDay(DayMax(prevMonth, GetYear()));
+                }
+                SetMonth(prevMonth);
+            }
+            else
+            {
+                SetMonth(12);
+                PreviousYear();
+            }
+            return this;
+        }
+
+        public MyDate PreviousYear()
+        {
+            int currentYear = GetYear();
+            if (currentYear > 1)
+            {
+                SetYear(currentYear - 1);
+
+                if (GetDay() > DayMax())
+                { SetDay(DayMax()); }
+
+                return this;
+            }
+            else
+            {
+                throw new InvalidOperationException("Year out of range!");
+            }
+        }
+
+        override public string ToString()
+        {
+            DAYS day = (DAYS)GetDayOfWeek(GetDay(), GetMonth(), GetYear());
+            return $"{day} {GetDay()} {GetMonth()} {GetYear()}";
+        }
+
+        static bool IsLeapYear(int year)
+        {
+            /* A year is a leap year if it is divisible by 4 but not by
+            100, or it is divisible by 400 */
+
+            if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+            { return true; }
+
+            return false;
+        }
+
+        bool IsValidDate(int day, int month, int year) // Reversed order to d, m, y.
+        {
+            int dayMax = DayMax(month, year);
+            if (year > 9999    || year < 1)       { return false; }
+            if (month > 12     || month < 1)      { return false; }
+            if (day > dayMax   || day < 1)        { return false; }
+
+            return true;
+        }
+
+        int GetDayOfWeek(int day, int month, int year)
+        {
+            /* tomohiko sakamoto algorithm. source:
+             *  https://www.geeksforgeeks.org/tomohiko-sakamotos-algorithm-finding-day-week/ */
+
+            int[] t = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
+
+            if (month < 3) { year -= 1; }
+
+            return (year + year / 4 - year / 100 + year / 400
+                                + t[month - 1] + day) % 7;
+        }
+        
+    }
+}
---
+++ TestMyDate.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace MyDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // MyDate object created with non-default attibutes
+            MyDate today = new(2, 2, 2);
+            Console.WriteLine(today.ToString());
+            Console.WriteLine();
+
+
+            // Incorrect arguments that should generate exceptions
+            Console.WriteLine("A series of incorrect arguments:");
+            today.SetDay(33);
+            today.SetMonth(13);
+            today.SetYear(0);
+            today.SetYear(10000);
+
+
+            // Shows date still the same after incorrect arguments
+            Console.WriteLine(today.ToString());
+            Console.WriteLine();
+
+
+            // NextDay method edge case
+            Console.WriteLine("NextDay:");
+            today.SetDate(31, 12, 2000);
+            Console.WriteLine(today.ToString());
+            today.NextDay();
+            Console.WriteLine(today.ToString());
+            Console.WriteLine();
+
+
+            // NextMonth method edge case
+            Console.WriteLine("NextMonth:");
+            today.SetDate(31, 10, 2012);
+            Console.WriteLine(today.ToString());
+            today.NextMonth();
+            Console.WriteLine(today.ToString());
+            Console.WriteLine();
+
+
+            // NextYear method edge case
+            Console.WriteLine("NextYear:");
+            today.SetDate(29, 2, 2012);
+            Console.WriteLine(today.ToString());
+            today.NextYear();
+            Console.WriteLine(today.ToString());
+            Console.WriteLine();
+
+
+            // PreviousDay method edge case
+            Console.WriteLine("PreviousDay:");
+            today.SetDate(1, 1, 2001);
+            Console.WriteLine(today.ToString());
+            today.PreviousDay();
+            Console.WriteLine(today.ToString());
+            Console.WriteLine();
+
+
+            // PreviousMonth method edge case
+            Console.WriteLine("PreviousMonth:");
+            today.SetDate(31, 10, 2012);
+            Console.WriteLine(today.ToString());
+            today.PreviousMonth();
+            Console.WriteLine(today.ToString());
+            Console.WriteLine();
+
+
+            // PreviousYear method edge case
+            Console.WriteLine("PreviousYear:");
+            today.SetDate(29, 2, 2012);
+            Console.WriteLine(today.ToString());
+            today.PreviousYear();
+            Console.WriteLine(today.ToString());
+            Console.WriteLine();
+        }
+    }
+}
