23db53cc5d5a06e3335fd98d7701dd7f541629c5
Lyndon Purcell <65317064+ljpurcell@users.noreply.github.com>
2022-07-31 16:48:10 +1000

    Upload generics and lambda problem set

---
+++ MyStack.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8._1_LambdaExpressions_And_Generics
+{
+    // Generic stack data structure and accompanying methods
+    public class MyStack<T>
+    {
+        private T[] array;
+        public int Count { get; private set; }
+
+        public MyStack(int capacity)
+        {
+            array = new T[capacity];
+            Count = 0;
+        }
+
+        public void Push(T val)
+        {
+            if (Count < array.Length) array[Count++] = val;
+            else throw new InvalidOperationException("The stack is out of capacity.");
+        }
+
+        public T Pop()
+        {
+            if (Count > 0) return array[--Count];
+            else throw new InvalidOperationException("The stack is empty.");
+        }
+
+        public T Find(Func<T, bool> criteria)
+        {
+            try
+            {
+                if (criteria == null) throw new ArgumentNullException($"Criteria parameter was null!");
+                for (int i = Count - 1; i >= 0; i--)
+                {
+                    if (criteria(array[i])) return array[i];
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+            return default;
+        }
+
+        public T[] FindAll(Func<T, bool> criteria)
+        {
+            try
+            {
+                if (criteria == null) throw new ArgumentNullException($"Criteria paramater was null!");
+
+                int trueElements = 0;
+                for (int i = Count - 1; i >= 0; i--)
+                {
+                    if (criteria(array[i])) trueElements++;
+                }
+
+                if (trueElements == 0) return null;
+
+                T[] newArray = new T[trueElements];
+                int addToNew = trueElements - 1;
+
+                for (int i = Count - 1; i >= 0; i--)
+                {
+                    if (criteria(array[i]))
+                    {
+                        newArray[addToNew--] = array[i];
+                    }
+                }
+                return newArray;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+            return default;
+        }
+
+        public int RemoveAll(Func<T, bool> criteria)
+        {
+            try
+            {
+                if (criteria == null) throw new ArgumentNullException($"Criteria parameter was null!");
+
+                int elementsToRemove = 0;
+                for (int i = Count - 1; i >= 0; i--)
+                {
+                    if (criteria(array[i])) elementsToRemove++;
+                }
+
+                if (elementsToRemove == 0) return 0;
+
+                int arrayResize = Count - elementsToRemove;
+                T[] copiedArray = (T[])array.Clone();
+                array = new T[arrayResize];
+
+                int newIndex = arrayResize - 1;
+                for (int i = copiedArray.Length - 1; i >= 0; i--)
+                {
+                    if (!criteria(copiedArray[i]))
+                    {
+                        array[newIndex--] = copiedArray[i];
+                    }
+                }
+                return elementsToRemove;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+            return default;
+        }
+
+        public T Max()
+        {
+            if (Count == 0) return default;
+            else
+            {
+                T currentMax = array[0];
+                Comparer<T> comparer = Comparer<T>.Default;
+                int result;
+
+                for (int i = 1; i < Count; i++)
+                {
+                    result = comparer.Compare(array[i], currentMax);
+                    if (result > 0) currentMax = array[i];
+                }
+
+                return currentMax;
+            }
+        }
+
+        public T Min()
+        {
+            if (Count == 0) return default;
+            else
+            {
+                T currentMin = array[0];
+                Comparer<T> comparer = Comparer<T>.Default;
+                int result;
+
+                for (int i = 1; i < Count; i++)
+                {
+                    result = comparer.Compare(array[i], currentMin);
+                    if (result < 0) currentMin = array[i];
+                }
+
+                return currentMin;
+            }
+        }
+    }
+}
---
+++ Tester.cs
@@ -0,0 +1,191 @@
+﻿using System;
+
+namespace _8._1_LambdaExpressions_And_Generics
+{
+    // For ease of method calling in Tester class
+    public delegate Account findDelegate(Func<Account, bool> criteria);
+
+    // For testing the above generic stack methods
+    class Tester
+    {
+        static void Main(string[] args)
+        {
+            Account smallestBalance = new("Littlest", 10m);
+            Account secondSmallestBalance = new("LittleMedium", 250m);
+            Account middleBalance = new("Medium", 500m);
+            Account secondLargestBalance = new("MediumLarge", 1000m);
+            Account largestBalance = new("Largest", 5000m);
+
+
+            // Create new stack object and add accounts to it smallest to largest 
+
+            MyStack<Account> AccountStack = new(5);
+            AccountStack.Push(smallestBalance);
+            AccountStack.Push(secondSmallestBalance);
+            AccountStack.Push(middleBalance);
+            AccountStack.Push(secondLargestBalance);
+            AccountStack.Push(largestBalance);
+
+
+            // Test for Find
+            Console.WriteLine("* Tests for 'Find' *");
+
+            findDelegate find = AccountStack.Find;
+
+            Func<Account, bool> nullCriteria = null;
+            Console.Write("Null criteria: ");
+            find(nullCriteria);
+
+            Func<Account, bool> hasMoney = (x) => x.Balance > 0;
+            Console.Write("First account with money: ");
+            Console.WriteLine(find(hasMoney).Name); 
+
+            Func<Account, bool> lessThanThreeFigures = (x) => x.Balance < 100;
+            Console.Write("First account with under $100: ");
+            Console.WriteLine(find(lessThanThreeFigures).Name); 
+
+            Func<Account, bool> hasOneGrand = (x) => x.Balance == 1000;
+            Console.Write("First account with exactly $1,000: ");
+            Console.WriteLine(find(hasOneGrand).Name); 
+
+            Func<Account, bool> atLeastFourFigures = (x) => x.Balance < 1000 && x.Balance > 99;
+            Console.Write("First account with less than $1,000 but more than $100: ");
+            Console.WriteLine(find(atLeastFourFigures).Name); 
+
+
+            // Test for FindAll
+
+            Console.WriteLine("\n* Tests for 'FindAll' *");
+
+            Account anotherSmallestBalance = new("AlsoLittlest", 10m);
+            Account sameNameAsSmallestBalance = new("Littlest", 10.5m);
+            Account oneThousandBalance = new("Grand1", 1000m);
+            Account oneThousandBalance2 = new("Grand2", 1000m);
+
+            void reMakeStack() // Reestablishes stack after manipulating it
+            {
+                AccountStack = new(9);
+                AccountStack.Push(smallestBalance);
+                AccountStack.Push(secondSmallestBalance);
+                AccountStack.Push(middleBalance);
+                AccountStack.Push(secondLargestBalance);
+                AccountStack.Push(largestBalance);
+                AccountStack.Push(anotherSmallestBalance);
+                AccountStack.Push(sameNameAsSmallestBalance);
+                AccountStack.Push(oneThousandBalance);
+                AccountStack.Push(oneThousandBalance2);
+            }
+
+            reMakeStack();
+
+
+            Account[] foundAll = AccountStack.FindAll(hasMoney);
+
+            Console.WriteLine("\n1. Has money:");
+            foreach(Account account in foundAll)
+            {
+                account.Print();
+            }
+
+            foundAll = AccountStack.FindAll(hasOneGrand); 
+
+            Console.WriteLine("\n2. Has $1,000:");
+            foreach (Account account in foundAll)
+            {
+                account.Print();
+            }
+
+            Func<Account, bool> hasTenDollars = (x) => x.Balance == 10;
+            foundAll = AccountStack.FindAll(hasTenDollars);
+
+            Console.WriteLine("\n3. Has $10:");
+            foreach (Account account in foundAll)
+            {
+                account.Print();
+            }
+
+            Func<Account, bool> hasNameLittlest = (x) => x.Name == "Littlest";
+            foundAll = AccountStack.FindAll(hasNameLittlest);
+
+            Console.WriteLine("\n4. Has name 'Littlest':");
+            foreach (Account account in foundAll)
+            {
+                account.Print();
+            }
+
+
+            Console.WriteLine("\n5. Null:");
+            foundAll = AccountStack.FindAll(nullCriteria);
+
+
+            // Test for RemoveAll
+
+            Console.WriteLine("\n* Tests for 'RemoveAll' *");
+
+            int itemsRemoved = AccountStack.RemoveAll(hasMoney);
+
+            Console.WriteLine("\n1. Has money:");
+            Console.WriteLine($"Items removed: {itemsRemoved}");
+
+            reMakeStack();
+
+            itemsRemoved = AccountStack.RemoveAll(hasOneGrand);
+
+            Console.WriteLine("\n2. Has $1,000:");
+            Console.WriteLine($"Items removed: {itemsRemoved}");
+
+            reMakeStack();
+
+            Console.WriteLine("\n3. Null:");
+            itemsRemoved = AccountStack.RemoveAll(nullCriteria);
+            Console.WriteLine($"Items removed: {itemsRemoved}");
+
+
+            // Test for Min
+
+            Console.WriteLine("\n* Test for 'Min' *");
+
+            Account minimum = AccountStack.Min();
+            minimum.Print();
+
+            AccountStack = new(10);
+            AccountStack.Push(smallestBalance);
+            AccountStack.Push(secondSmallestBalance);
+            AccountStack.Push(middleBalance);
+            AccountStack.Push(secondLargestBalance);
+            AccountStack.Push(largestBalance);
+            AccountStack.Push(anotherSmallestBalance);
+            AccountStack.Push(sameNameAsSmallestBalance);
+            AccountStack.Push(oneThousandBalance);
+            AccountStack.Push(oneThousandBalance2);
+            AccountStack.Push(new Account("El Cheapo", 0.1m)); // New minimum element
+
+            minimum = AccountStack.Min();
+            minimum.Print();
+
+
+            // Test for Max
+
+            Console.WriteLine("\n* Test for 'Max' *");
+
+            Account maximum = AccountStack.Max();
+            maximum.Print();
+
+            AccountStack = new(10);
+            AccountStack.Push(smallestBalance);
+            AccountStack.Push(secondSmallestBalance);
+            AccountStack.Push(middleBalance);
+            AccountStack.Push(secondLargestBalance);
+            AccountStack.Push(largestBalance);
+            AccountStack.Push(anotherSmallestBalance);
+            AccountStack.Push(sameNameAsSmallestBalance);
+            AccountStack.Push(oneThousandBalance);
+            AccountStack.Push(oneThousandBalance2);
+            AccountStack.Push(new Account("El Richo", 10000000m)); // New maximum element
+
+            maximum = AccountStack.Max();
+            maximum.Print();
+
+        }
+    }
+}
